79ab693eb3f1ee9a311cdb7e8eb30fb1c214fc91
aleankas <rustiantoandi17@gmail.com>
2020-12-05 01:51:03 +0700

    view product menu

--- cashierx01/src/App.js
+++ cashierx01/src/App.js
@@ -1,13 +1,54 @@
-import React from "react";
-import { NavbarComponent } from "./components/NavbarComponent";
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { NavbarComponent, ListCategories, Hasil, Menus } from "./components";
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <h1>Hello world</h1>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products")
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const menus = this.state.menus;
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>Daftar Produk</h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((itemMenu) => (
+                      <Menus key={itemMenu.id} menu={itemMenu} />
+                    ))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ cashierx01/src/components/Menus.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+import { numberFormatWithCommas } from "../utils/numberFormatWithCommas";
+
+const Menus = ({ menu }) => {
+  return (
+    <Col md={4} xs={6} className="mb-4">
+      <Card className="shadow">
+        <Card.Img
+          variant="top"
+          src={
+            "assets/images/" +
+            menu.category.nama.toLowerCase() +
+            "/" +
+            menu.gambar
+          }
+        />
+        <Card.Body>
+          <Card.Title>
+            {menu.nama} [{menu.kode}]
+          </Card.Title>
+          <Card.Text>Rp. {numberFormatWithCommas(menu.harga)}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Menus;
